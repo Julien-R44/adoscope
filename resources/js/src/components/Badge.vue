d6fac57b3c733f55ad9b9e0fd4b7b4313a7d00d4
Julien <julien@ripouteau.com>
2021-12-31 17:59:14 +0100

    ✨ dark mode support for badge component

--- resources/js/src/components/Badge.vue
+++ resources/js/src/components/Badge.vue
@@ -1,10 +1,10 @@
 <template>
   <div :class="{
     'px-2 py-1 inline-block rounded-md font-medium': true,
-    'bg-yellow-200': type === 'warning',
-    'bg-red-200': type === 'danger',
-    'bg-blue-200': type === 'info',
-    'bg-green-200': type === 'secondary',
+    'bg-yellow-200 dark:bg-yellow-500': type === 'warning',
+    'bg-red-200 dark:bg-red-500': type === 'danger',
+    'bg-blue-200 dark:bg-blue-500': type === 'info',
+    'bg-green-200 dark:bg-green-500': type === 'secondary',
   }">
     <slot></slot>
   </div>
