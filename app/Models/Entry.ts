bebbfc64fe641bfd13128ddc82668a0f629f22a5
Julien <julien@ripouteau.com>
2021-12-31 05:05:03 +0100

    ✨ added entry model

---
+++ app/Models/Entry.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, column } from '@ioc:Adonis/Lucid/Orm'
+import { v4 as uuid } from 'uuid'
+import { EntryType } from 'App/types'
+
+export default class Entry extends BaseModel {
+  public static table = 'adoscope_entries'
+  public static selfAssignPrimaryKey = true
+
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public type: EntryType
+
+  @column()
+  public content: Record<string, any>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @beforeCreate()
+  public static async createUuid(model: Entry) {
+    model.id = uuid()
+  }
+}
