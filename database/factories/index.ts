6bc3b52a56194d531b755750c9c535e72b4723cf
Julien <julien@ripouteau.com>
2021-12-31 22:20:47 +0100

    ✨ added some props to factories

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -12,12 +12,15 @@ const RequestPropsFactory = (faker: Faker.FakerStatic) => ({
     ip_address: faker.internet.ip(),
     uri: faker.internet.url(),
     method: faker.random.arrayElement(['GET', 'POST', 'PUT', 'DELETE']),
+    controller_action: `${faker.lorem.word()}Controller.${faker.lorem.word()}`,
     middleware: faker.datatype.boolean() ? faker.random.words(2) : null,
     headers: {
       'accept': faker.random.arrayElement(['application/json', 'application/xml']),
       'user-agent': faker.internet.userAgent(),
       [faker.lorem.word()]: faker.lorem.words(),
     },
+    payload: { [faker.lorem.word()]: faker.lorem.words() },
+    response: { [faker.lorem.word()]: faker.lorem.words() },
     response_status: faker.datatype.number({ min: 200, max: 500 }),
     duration: faker.datatype.number({ min: 0, max: 1000 }),
   },
@@ -37,6 +40,9 @@ const CommandPropsFactory = (faker: Faker.FakerStatic) => ({
       [faker.lorem.word()]: faker.lorem.words(),
       [faker.lorem.word()]: faker.datatype.boolean(),
     },
+    arguments: {
+      [faker.lorem.word()]: faker.datatype.boolean(),
+    },
   },
   created_at: DateTime.fromJSDate(faker.date.recent(1)),
 })
@@ -48,7 +54,15 @@ const QueryPropsFactory = (faker: Faker.FakerStatic) => ({
   type: EntryType.QUERY,
   content: {
     connection: faker.random.arrayElement(['mysql', 'sqlite', 'postgres']),
-    sql: faker.lorem.sentence(),
+    sql: faker.random.arrayElement([
+      `SELECT EMP_ID, NAME FROM EMPLOYEE_TBL WHERE EMP_ID = '0000';`,
+      `SELECT EMP_ID, LAST_NAME FROM EMPLOYEE WHERE CITY = 'Seattle' ORDER BY EMP_ID;`,
+      `CREATE VIEW Failing_Students AS SELECT S_NAME, Student_ID FROM STUDENT WHERE GPA > 40;`,
+      `CREATE OR REPLACE VIEW [ Product List ] AS SELECT ProductID, ProductName, Category FROM Products WHERE Discontinued = No;`,
+      `SELECT Name FROM Customers WHERE EXISTS
+      (SELECT Item FROM Orders
+      WHERE Customers.ID = Orders.ID AND Price < 50)`,
+    ]),
     bindings: faker.lorem.words(),
     time: faker.datatype.number({ min: 0, max: 1000 }),
   },
