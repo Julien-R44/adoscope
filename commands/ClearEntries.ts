5bccc78ba7298e7762a0aa7bf6b01c55ea3630d5
Julien <julien@ripouteau.com>
2021-12-31 23:44:41 +0100

    ✨ added clear entries command

---
+++ commands/ClearEntries.ts
@@ -0,0 +1,27 @@
+import { BaseCommand } from '@adonisjs/core/build/standalone'
+import { AdoscopeConfig } from 'Config/adoscope'
+
+export default class ClearEntries extends BaseCommand {
+  public static commandName = 'adoscope:clear'
+  public static description = 'Clear all entries from Adoscope'
+
+  public static settings = {
+    loadApp: true,
+  }
+
+  public async run() {
+    const { default: Database } = await import('@ioc:Adonis/Lucid/Database')
+    const { default: Config } = await import('@ioc:Adonis/Core/Config')
+    const adoscopeConfig = Config.get('adoscope') as AdoscopeConfig
+
+    const spinner = this.logger.await('Clearing entries from database...')
+
+    await Database.connection(adoscopeConfig.storage.databaseConnection).truncate(
+      'adoscope_entries',
+      true
+    )
+
+    this.logger.success('All entries have been cleared.')
+    spinner.stop()
+  }
+}
