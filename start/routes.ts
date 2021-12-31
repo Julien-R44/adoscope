9f297d815f7897b545cb775fa863731b6ee98473
Julien <julien@ripouteau.com>
2021-12-31 23:07:48 +0100

    ♻️ clean adonis router

--- start/routes.ts
+++ start/routes.ts
@@ -1,29 +1,5 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer''
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
-
-Route.get('/telescope', async ({ view }) => view.render('telescope'))
+Route.get('/telescope', ({ view }) => view.render('telescope'))
 Route.get('/telescope-api/entries', 'EntriesController.index')
 Route.get('/telescope-api/entries/:id', 'EntriesController.show')
