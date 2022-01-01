a2878f55333d13d9d21fba60a0a2da5b3e20cbfa
Julien <julien@ripouteau.com>
2022-01-01 01:12:42 +0100

    ✨ support config.middlewares

--- start/routes.ts
+++ start/routes.ts
@@ -4,6 +4,8 @@ import { AdoscopeConfig } from 'Config/adoscope'
 
 const adoscopeConfig = Config.get('adoscope') as AdoscopeConfig
 
-Route.get(adoscopeConfig.path, ({ view }) => view.render('telescope'))
+Route.get(adoscopeConfig.path, ({ view }) => view.render('telescope')).middleware(
+  adoscopeConfig.middlewares
+)
 Route.get('/telescope-api/entries', 'EntriesController.index')
 Route.get('/telescope-api/entries/:id', 'EntriesController.show')
